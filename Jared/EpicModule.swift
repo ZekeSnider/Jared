7fcf84ba1ab5b8c5e9869ba41c79e61dd40abefe
Zeke Snider <zekesnider@me.com>
2016-04-15 15:11:09 +0900

    Fixed EpicModule!!!!

--- Jared/EpicModule.swift
+++ Jared/EpicModule.swift
@@ -5,62 +5,64 @@
 //  Created by Jared Derulo on 4/12/16.
 //  Copyright © 2016 Dylan Gustafson & Zeke Snider. All rights reserved.
 //
+
 import Foundation
 import Cocoa
-struct EpicModule: RoutingModule {
+class EpicModule: RoutingModule {
     var routes: [Route] = []
+    var description = "fucLk"
     
     init() {
-        let fuccboi = Route(comparisons: [.StartsWith: "/fuccboi"], call: self.youreFuccboi)
-        let tip = Route(comparisons: [.StartsWith: "/tip"], call: self.tipBuddy)
-        let bazinga = Route(comparisons: [.StartsWith: "/bazinga"], call: self.sendBazing)
-        let nice = Route(comparisons: [.StartsWith: "/nice"], call: self.sendNice)
-        let stop = Route(comparisons: [.Contains: "stop"], call: self.dontStop)
-        let hello = Route(comparisons: [.Contains: "hello"], call: self.sendHello)
-        let rate = Route(comparisons: [.StartsWith: "/10"], call: self.rateMessage)
-        let bigBunny = Route(comparisons: [.Contains: "Hey jared please send me the big buck bunny movie trailer"], call: self.bigBunny)
-        let slots = Route(comparisons: [.StartsWith: "/slots"], call: self.spinSlots)
-        let kill = Route(comparisons: [.StartsWith: "/kill"], call: self.killJared)
-        let clear = Route(comparisons: [.StartsWith: "/clear"], call: self.clearChat)
-        let kin = Route(comparisons: [.StartsWith: "/kin"], call: self.getKin)
-        let correction = Route(comparisons: [.StartsWith: "*"], call: self.correct)
-        let done = Route(comparisons: [.StartsWith: "/done"], call: self.donezo)
-        let choose = Route(comparisons: [.StartsWith: "/choose"], call: self.makeChoice)
-        let guess = Route(comparisons: [.StartsWith: "/guess"], call: self.guessNumber)
-        let jew = Route(comparisons: [.StartsWith: "/jew"], call: self.printJew)
+        let fuccboi = Route(comparisons: [.StartsWith: "/fuccboi"], call: self.youreFuccboi, description: "Call Jared a fuccboi")
+        let tip = Route(comparisons: [.StartsWith: "/tip"], call: self.tipBuddy, description: "Tip your fedora to a fellow gentlesir", parameterSyntax: ["/tip,[Name of friend (optional)]"])
+        let bazinga = Route(comparisons: [.StartsWith: "/bazinga"], call: self.sendBazing, description: "Become epic like sheldon cooper")
+        let nice = Route(comparisons: [.StartsWith: "/nice"], call: self.sendNice, description: "Congratulate someone on an epic accomplishment")
+        let stop = Route(comparisons: [.Contains: "stop"], call: self.dontStop, description: "Stop Jared")
+        let hello = Route(comparisons: [.Contains: "hello"], call: self.sendHello, description: "Say hello to Jared")
+        let rate = Route(comparisons: [.StartsWith: "/10"], call: self.rateMessage, description: "Rate out of 10")
+        let bigBunny = Route(comparisons: [.Contains: "Hey jared please send me the big buck bunny movie trailer"], call: self.bigBunny, description: "Send the big buck bunny movie trailer")
+        let slots = Route(comparisons: [.StartsWith: "/slots"], call: self.spinSlots, description: "Play slots")
+        let kill = Route(comparisons: [.StartsWith: "/kill"], call: self.killJared, description: "Kill Jared")
+        let clear = Route(comparisons: [.StartsWith: "/clear"], call: self.clearChat, description: "Clear the chat")
+        
+        let kin = Route(comparisons: [.StartsWith: "/kin"], call: self.getKin, description: "Discover your other-kin")
+        let correction = Route(comparisons: [.StartsWith: "*"], call: self.correct, description: "Handy text correction utility")
+        let done = Route(comparisons: [.StartsWith: "/done"], call: self.donezo, description: "Declare your completion of a task")
+        let choose = Route(comparisons: [.StartsWith: "/choose"], call: self.makeChoice, description: "Make a tough decision easily")
+        let guess = Route(comparisons: [.StartsWith: "/guess"], call: self.guessNumber, description: "Fun Number guessing game")
+        let jew = Route(comparisons: [.StartsWith: "/jew"], call: self.printJew, description: "Jew")
         
         routes = [fuccboi, tip, bazinga, nice, stop, hello, rate, bigBunny, slots, kill, clear, kin, correction, done, choose, guess, jew]
     }
     
     func bigBunny(message:String, myRoom: Room) -> Void{
-    SendText("Sure thing, buddy!", toRoom: myRoom)
-    if let MoviePath = NSBundle.mainBundle().pathForResource("Big_Buck_Bunny_Trailer", ofType: "m4v") {
-    SendImage(MoviePath, toRoom: myRoom)
-    }
-    }
-    
-    func youreFuccboi(message:String, myRoom: Room) -> Void{
-    if let theBuddy = myRoom.buddyName {
-    SendText("No \(theBuddy), you're the fuccboi", toRoom: myRoom)
+        SendText("Sure thing, buddy!", toRoom: myRoom)
+        if let MoviePath = NSBundle.mainBundle().pathForResource("Big_Buck_Bunny_Trailer", ofType: "m4v") {
+            SendImage(MoviePath, toRoom: myRoom)
+        }
     }
     
+    func youreFuccboi(message:String, myRoom: Room) -> Void {
+        if let theBuddy = myRoom.buddyName {
+            SendText("No \(theBuddy), you're the fuccboi", toRoom: myRoom)
+        }
     }
     
     func tipBuddy(message:String, myRoom: Room) -> Void {
-            let FedoPath = NSBundle.mainBundle().pathForResource("Fedora", ofType: "jpg")
-            let parameters = message.componentsSeparatedByString(",")
-            let tipText = "I tip my fedora to thee!"
+        let FedoPath = NSBundle.mainBundle().pathForResource("Fedora", ofType: "jpg")
+        let parameters = message.componentsSeparatedByString(",")
+        let tipText = "I tip my fedora to thee!"
             
-            if let FedoImage = FedoPath {
+        if let FedoImage = FedoPath {
             SendImage(FedoImage, toRoom: myRoom)
-            }
+        }
             
-            if let tipReceiver = parameters[safe: 1] {
+        if let tipReceiver = parameters[safe: 1] {
             SendText("\(tipReceiver), \(tipText)", toRoom: myRoom)
         }
-            else {
+        else {
             SendText("\(tipText)", toRoom: myRoom)
-            }
+        }
     }
     
     func sendBazing(message:String, myRoom: Room) -> Void{
@@ -75,9 +77,9 @@ struct EpicModule: RoutingModule {
     
     func dontStop(message:String, myRoom: Room) -> Void {
         if let theBuddy = myRoom.buddyName {
-        for _ in 1...5 {
-        SendText("Fuck off \(theBuddy) I dont stop for anyone", toRoom: myRoom)
-        }
+            for _ in 1...5 {
+                SendText("Fuck off \(theBuddy) I dont stop for anyone", toRoom: myRoom)
+            }
         }
     }
     
@@ -106,30 +108,30 @@ struct EpicModule: RoutingModule {
                 let s4 = Int(arc4random_uniform(500))
                 
                 if s4 == 100
-    {
-        SendText("JEW | JEW | JEW\nYOU WIN!!!", toRoom: myRoom)
-    }
+        {
+            SendText("JEW | JEW | JEW\nYOU WIN!!!", toRoom: myRoom)
+        }
         else {
-        SendText("\(s1) | \(s2) | \(s3)", toRoom: myRoom)
-        if s1 == 7 && s2 == 7 && s3 == 7 {
-            SendText("Nice! You get the Jared seal of approval", toRoom: myRoom)
+            SendText("\(s1) | \(s2) | \(s3)", toRoom: myRoom)
+            if s1 == 7 && s2 == 7 && s3 == 7 {
+                SendText("Nice! You get the Jared seal of approval", toRoom: myRoom)
+            }
+            if s1 == 6 && s2 == 6 && s3 == 6 {
+            SendText("lol u r the devil", toRoom: myRoom)
+            }
         }
-        if s1 == 6 && s2 == 6 && s3 == 6 {
-        SendText("lol u r the devil", toRoom: myRoom)
-        }
-                }
     }
     
     func killJared(message:String, myRoom: Room) -> Void {
         if let theBuddy = myRoom.buddyName {
-        SendText("fite me irl \(theBuddy)", toRoom: myRoom)
+            SendText("fite me irl \(theBuddy)", toRoom: myRoom)
         }
     }
     
     func clearChat(message:String, myRoom: Room) -> Void {
-                SendText("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n:^)", toRoom: myRoom)
+        SendText("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n:^)", toRoom: myRoom)
     }
-}
+
     func getKin(message:String, myRoom: Room) -> Void {
         let randoKin = "cat" //for now
         if let theBuddy = myRoom.buddyName {
@@ -172,7 +174,7 @@ struct EpicModule: RoutingModule {
     var secretNum: Int = 0
     var guesses: Int = -1
     
-    func guessNumber message:String, myRoom: Room) -> Void {
+    func guessNumber(message:String, myRoom: Room) -> Void {
     
         let parameters = message.componentsSeparatedByString(",")
     
@@ -207,8 +209,9 @@ struct EpicModule: RoutingModule {
                         SendText("Higher", toRoom: myRoom)
                     }
                     else {
-                        SendText("OMG \(myRoom.buddyName) Wins! :^)", toRoom: myRoom)
-                        SendText("It took you \(guesses) guesses", toRoom: myRoom)
+                        if let buddyName = myRoom.buddyName {
+                            SendText("OMG \(buddyName) Wins! :^) \n\n It took you \(guesses) guesses", toRoom: myRoom)
+                        }
                         guesses = -1
                     }
                 }
@@ -234,6 +237,7 @@ struct EpicModule: RoutingModule {
     }
     
     func printJew(message:String, myRoom: Room) -> Void  {
-        SendText("           ✡\n          ✡  ✡\n✡✡✡✡✡✡✡✡✡✡✡\n ✡  ✡           ✡  ✡\n   ✡               ✡\n ✡  ✡          ✡  ✡\n✡✡✡✡✡✡✡✡✡✡✡\n          ✡  ✡\n            ✡", toRoom: myRoom)
+        //This is not anti-semetic, this ia tribute to our fellow sons of god. <no sarcasm>
+        SendText("            ✡\n          ✡  ✡\n✡✡✡✡✡✡✡✡✡✡✡\n  ✡  ✡           ✡  ✡\n   ✡               ✡\n  ✡  ✡           ✡  ✡\n✡✡✡✡✡✡✡✡✡✡✡\n          ✡  ✡\n            ✡", toRoom: myRoom)
     }
 }
