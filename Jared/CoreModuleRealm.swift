014973eb60665afbf32f0e501e2d980d00ec64e1
Zeke Snider <zekesnider@me.com>
2017-10-24 22:42:56 -0700

    Changes to realm file

--- Jared/CoreModuleRealm.swift
+++ Jared/CoreModuleRealm.swift
@@ -1,21 +1,41 @@
 import Foundation
 import RealmSwift
 
-class JalenQuote: Object {
-    dynamic var text = ""
+enum IntervalType: String {
+    case Minute
+    case Hour
+    case Day
+    case Week
+    case Month
 }
 
-class BazingaLine: Object {
-    dynamic var text = ""
-}
+let intervalSeconds: [IntervalType: Double] =
+[
+    .Minute: 60.0,
+    .Hour: 3600.0,
+    .Day: 86400.0,
+    .Week: 604800.0,
+    .Month: 2592000.0
+]
 
-class Dream: Object {
+class SchedulePost: Object {
+    dynamic var sendIntervalNumber = 1
+    dynamic var sendIntervalType = IntervalType.Hour.rawValue
     dynamic var text = ""
-    dynamic var submitter = ""
-    dynamic var date: Date? = nil
-}
-
-class Joke: Object {
-    dynamic var text = ""
-    dynamic var submitter: String? = ""
+    dynamic var handle = ""
+    dynamic var sendNumberTimes = 1
+    dynamic var chatID = ""
+    dynamic var startDate = Date()
+    dynamic var id = UUID().uuidString
+    dynamic var lastSendDate = Date()
+    
+    var sendIntervalTypeEnum: IntervalType {
+        get {
+            return IntervalType(rawValue: sendIntervalType)!
+        }
+        set {
+            sendIntervalType = newValue.rawValue
+        }
+    }
+    
 }
