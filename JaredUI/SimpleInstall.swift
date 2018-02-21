b9620059cd1fb70e4c55d97ab4303bcc4c8ab3ef
Zeke Snider <zekesnider@me.com>
2018-02-20 22:43:23 -0800

    Add to simple install

--- JaredUI/SimpleInstall.swift
+++ JaredUI/SimpleInstall.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+@discardableResult
+func shell(_ args: String...) -> Int32 {
+    let task = Process()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+    task.launch()
+    task.waitUntilExit()
+    return task.terminationStatus
+}
+
+struct SimpleInstall {
+    func Install() {
+        copyFile()
+        shell("killall", "Messages")
+        shell ("defaults", "write", "~/Library/Containers/com.apple.soagent/Data/Library/Preferences/com.apple.messageshelper.AlertsController.plist", "AppleScriptNameKey", "-string", "\"Jared.applescript\"")
+        shell("open", "/Applications/Messages.app")
+        /*
+        let myPath = NSString(string: "~/Library/Containers/com.apple.soagent/Data/Library/Preferences/com.apple.messageshelper.AlertsController.plist").expandingTildeInPath
+        if let dict = NSMutableDictionary(contentsOfFile: myPath) {
+            dict["AppleScriptNameKey"] = "Jared.applescript"
+            dict.write(toFile: myPath, atomically: true )
+        }*/
+        
+        
+        
+        
+    }
+    
+    func copyFile() {
+        let appSupportFilePath = URL(fileURLWithPath: NSString(string: "~/Library/Application Scripts/com.apple.iChat/").expandingTildeInPath).appendingPathComponent("Jared.applescript").path
+        
+        let bundlePath = Bundle.main.path(forResource: "Jared", ofType: ".applescript")
+        
+        let fileManager = FileManager.default
+        
+        do {
+            try fileManager.copyItem(atPath: bundlePath!, toPath: appSupportFilePath)
+        } catch {
+            print("\n")
+            print(error)
+        }
+    }
+}
