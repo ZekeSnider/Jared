db8362b19d3909aa00249c0614a9c4137ac7263a
Zeke Snider <zekesnider@me.com>
2018-11-26 22:01:17 -0800

    Working query for messages from database

--- Jared/sqlitetest.swift
+++ Jared/sqlitetest.swift
@@ -12,16 +12,54 @@ import SQLite3
 class SqliteTest {
     static func test() {
         let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            .appendingPathComponent("test.sqlite")
+            .appendingPathComponent("chat.db")
         
         var db: OpaquePointer?
         if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
             print("error opening database")
         }
         
-        if sqlite3_exec(db, "create table if not exists test (id integer primary key autoincrement, name text)", nil, nil, nil) != SQLITE_OK {
+        let query = """
+            SELECT handle.id, message.text, datetime('now')
+                FROM message INNER JOIN handle
+                ON message.handle_id = handle.ROWID
+                WHERE is_from_me=0 AND
+                datetime(message.date/1000000000 + strftime("%s", "2001-01-01") ,"unixepoch","localtime") >= datetime('2017-11-27 05:05:16');
+        """
+        
+        var statement: OpaquePointer?
+        
+        if sqlite3_prepare_v2(db, query, -1, &statement, nil) != SQLITE_OK {
             let errmsg = String(cString: sqlite3_errmsg(db)!)
-            print("error creating table: \(errmsg)")
+            print("error preparing select: \(errmsg)")
         }
+        
+        while sqlite3_step(statement) == SQLITE_ROW {
+            guard let id = sqlite3_column_text(statement, 0) else {
+                break
+            }
+            guard let cString = sqlite3_column_text(statement, 1) else {
+                break
+            }
+            
+            let idString = String(cString: id)
+            print("id = \(idString); ", terminator: "")
+            
+            let name = String(cString: cString)
+            print("name = \(name)")
+        }
+        
+        if sqlite3_finalize(statement) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error finalizing prepared statement: \(errmsg)")
+        }
+        
+        statement = nil
+        
+        if sqlite3_close(db) != SQLITE_OK {
+            print("error closing database")
+        }
+        
+        db = nil
     }
 }
