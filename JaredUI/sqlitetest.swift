42802b13f2dd243e9bc6456c307e6d46a9424640
Zeke Snider <zekesnider@me.com>
2018-12-21 19:24:24 -0800

    SQLite is working

--- JaredUI/sqlitetest.swift
+++ JaredUI/sqlitetest.swift
@@ -9,12 +9,13 @@
 internal let SQLITE_STATIC = unsafeBitCast(0, to: sqlite3_destructor_type.self)
 internal let SQLITE_TRANSIENT = unsafeBitCast(-1, to: sqlite3_destructor_type.self)
 
+import Cocoa
+import JaredFramework
 import Foundation
-import SQLite3
 import SQLite3
 class SqliteTest {
     var db: OpaquePointer?
-    var querySinceDate: String?
+    var querySinceID: String?
     var shouldExitThread = false
     var refreshSeconds = 5.0
     
@@ -25,9 +26,7 @@ class SqliteTest {
             print("error opening database")
         }
         
-        let formatter: DateFormatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        querySinceDate = formatter.string(from: Date())
+        querySinceID = getCurrentMaxRecordID()
     }
     
     
@@ -58,15 +57,41 @@ class SqliteTest {
         backgroundAction()
     }
     
+    private func getCurrentMaxRecordID() -> String {
+        let query = "SELECT MAX(rowID) FROM message"
+        var id: String?
+        var statement: OpaquePointer?
+        
+        if sqlite3_prepare_v2(db, query, -1, &statement, nil) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing select: \(errmsg)")
+        }
+        
+        while sqlite3_step(statement) == SQLITE_ROW {
+            guard let idcString = sqlite3_column_text(statement, 0) else {
+                break
+            }
+            
+            id = String(cString: idcString)
+        }
+        
+        if sqlite3_finalize(statement) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error finalizing prepared statement: \(errmsg)")
+        }
+        
+        return id ?? "10000000000000"
+    }
+    
     private func queryNewRecords() -> Double {
         let start = Date()
         
         let query = """
-            SELECT handle.id, message.text, datetime('now', 'localtime')
+            SELECT handle.id, message.text, message.ROWID
                 FROM message INNER JOIN handle
                 ON message.handle_id = handle.ROWID
                 WHERE is_from_me=1 AND
-                datetime(message.date/1000000000 + strftime("%s", "2001-01-01") ,"unixepoch","localtime") >= datetime(?);
+                message.ROWID > ?
         """
         
         var statement: OpaquePointer?
@@ -76,7 +101,7 @@ class SqliteTest {
             print("error preparing select: \(errmsg)")
         }
         
-        if sqlite3_bind_text(statement, 1, querySinceDate ?? "3000-11-27 05:05:16", -1, SQLITE_TRANSIENT) != SQLITE_OK {
+        if sqlite3_bind_text(statement, 1, querySinceID ?? "1000000000", -1, SQLITE_TRANSIENT) != SQLITE_OK {
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             print("failure binding foo: \(errmsg)")
         }
@@ -87,20 +112,23 @@ class SqliteTest {
             }
             let id = String(cString: idcString)
             
-            guard let namecString = sqlite3_column_text(statement, 1) else {
+            guard let textcString = sqlite3_column_text(statement, 1) else {
                 break
             }
-            let name = String(cString: namecString)
+            let text = String(cString: textcString)
             
-            guard let currentTimeCString = sqlite3_column_text(statement, 2) else {
+            guard let rowIDcString = sqlite3_column_text(statement, 2) else {
                 break
             }
+            let rowID = String(cString: rowIDcString)
             
-            querySinceDate = String(cString: currentTimeCString)
+            querySinceID = rowID;
             
             print("id = \(id)")
-            print("name = \(name)")
-            print("currentTime = \(String(describing: querySinceDate))")
+            print("text = \(text)")
+            if let appDelegate = NSApplication.shared.delegate as? AppDelegate {
+                appDelegate.Router.routeMessage(text, fromBuddy: id, forRoom: Room(GUID: nil, buddyName: "", buddyHandle:  id))
+            }
         }
         
         if sqlite3_finalize(statement) != SQLITE_OK {
