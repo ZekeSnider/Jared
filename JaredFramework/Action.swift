d7509a73e471221f33374bfa0adc427c1b8000ea
Zeke Snider <zekesnider@me.com>
2020-08-04 22:59:27 -0700

    Finish logic for reactions, and add remove event

--- JaredFramework/Action.swift
+++ JaredFramework/Action.swift
@@ -10,6 +10,7 @@ import Foundation
 
 public enum ActionType: String {
     case like = "like"
+    case dislike = "dislike"
     case love = "love"
     case laugh = "laugh"
     case exclaim = "exclaim"
@@ -18,6 +19,16 @@ public enum ActionType: String {
     
     public init(fromActionTypeInt actionTypeInt: Int) {
         switch(actionTypeInt) {
+        case 2000:
+            self = .love
+        case 2001:
+            self = .like
+        case 2002:
+            self = .dislike
+        case 2003:
+            self = .laugh
+        case 2004:
+            self = .exclaim
         case 2005:
             self = .question
         default:
@@ -26,13 +37,32 @@ public enum ActionType: String {
     }
 }
 
+public enum ActionEvent: String {
+    case placed = "placed"
+    case removed = "removed"
+}
+
 public struct Action: Encodable {
 	public var type: ActionType
+    public var event: ActionEvent
 	public var targetGUID: String
 	
 	enum CodingKeys : String, CodingKey{
         case type
 		case targetGUID
+        case event
+    }
+    
+    public init(actionTypeInt: Int, targetGUID: String) {
+        if (actionTypeInt >= 3000) {
+            event = .removed
+            self.type = ActionType(fromActionTypeInt: actionTypeInt - 1000)
+        } else {
+            event = .placed
+            self.type = ActionType(fromActionTypeInt: actionTypeInt)
+        }
+        
+        self.targetGUID = targetGUID
     }
     
     public func encode(to encoder: Encoder) throws {
@@ -40,5 +70,6 @@ public struct Action: Encodable {
 		
 		try container.encode(type.rawValue, forKey: .type)
 		try container.encode(targetGUID, forKey: .targetGUID)
+        try container.encode(event.rawValue, forKey: .event)
 	}
 }
